eb79eab81729979765c750b1dd7dad8ecb58f035
MajoGro <62608335+MajoGro@users.noreply.github.com>
2020-09-30 12:24:41 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream> // to je ukaz ki pove da uporabljamo knjižnjico - Library
+
+using namespace std;
+
+int main(){
+
+// std :: cout<<"Hello World from Slovenia">>; ta zapis poenostavimo kot je zapisano v tej kodi
+
+cout<<"Hello World from Slovenia 111"<<endl;
+
+return 0;
+
+}
